1526c8a129deb7d38ac9498975815d0d6cacf26e
catalinaximenavaldes-cmd <catalina.ximena.valdes@gmail.com>
2026-03-21 18:51:29 -0300

    Add files via upload

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 // url.js
 const CONFIG = {
     // Pega aquí la URL de tu implementación de Apps Script
-    API_URL: "https://script.google.com/macros/s/AKfycbz8-7LojRxxVZdPjS8CnpV6U99udBDKZbNqNbsnHKCAA4SbVDRAmwE9EU7CCoK1TwDV/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycby4AwgWtHMKw8yq0W-mFObQql5vzMVmQSuQSr0z-V8Ro-_97in3zrITo_Ohcp5O3acK/exec",
 
     // Puedes agregar más configuraciones aquí si las necesitas en el futuro
     VERSION: "1.0.1"
